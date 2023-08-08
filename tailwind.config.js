26b8d78470ca92afe3f35525d22f23a440d350a7
valdimir-makarov <2010049@student.ruet.ac.bd>
2023-08-08 06:30:49 +0600

    my commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        'color-content':'#F85559',
+      },
+    },
+    plugins: [],
+  }
+  
